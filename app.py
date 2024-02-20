d1939719cc11667140d6860cb88194594b40b5fd
harishappana1 <52353094+harishappana1@users.noreply.github.com>
2024-02-20 19:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! from Python Program..............This is version V0.1'
+    return 'Hello, World! from Python Program..............This is version V0.2'
 
 if __name__ == '__main__':
     app.run(debug=True)
